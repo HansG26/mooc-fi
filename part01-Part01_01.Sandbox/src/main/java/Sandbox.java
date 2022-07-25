e4b7b8702df2f65acaaec5d1e867a39442611cd5
Hans Goos <gooshans26@gmail.com>
2022-07-25 20:59:16 +0200

    complete exercise part01-Part01_26.Orwell

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 public class Sandbox {
 
     public static void main(String[] args) {
+        int number = 55;
         
+        if (number != 0) {
+            System.out.println("The number is not equal to 0");
+        }
+        
+        if (number >= 1000) {
+            System.out.println("The number is at least 1000");
+        }
     }
 }
