d9e62da5b577b4c833a68aa13fbd71f598e50477
Hans Goos <gooshans26@gmail.com>
2022-07-25 20:45:31 +0200

    complete exercise part01-Part01_24.SpeedingTicket

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 public class Sandbox {
 
     public static void main(String[] args) {
-        int x = 10;
         
-        System.out.println("The value of the variable x is " + x);
-        
-        int y = 5;
-        int z = 6;
-        
-        System.out.println("y is " + y + " and z is " + z);
     }
 }
