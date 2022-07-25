1545ef13fc90184693c8f75da5f6f5a179845c7b
Hans Goos <gooshans26@gmail.com>
2022-07-25 08:33:42 +0200

    create git repository

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        System.out.println("here is an integer ---> " + 2);
+        System.out.println(2 + "<--- here is an integer");
+        
+        
+    }
+}
