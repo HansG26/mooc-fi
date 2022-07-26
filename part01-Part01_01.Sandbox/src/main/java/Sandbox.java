85e12a10d24468d9fad7b9399d6b3560dd87a79d
Hans Goos <gooshans26@gmail.com>
2022-07-26 19:42:42 +0200

    complete exercise part01-Part01_33.Password

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -6,12 +6,23 @@ public class Sandbox {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         
-        int number = Integer.valueOf(reader.nextLine());
+        System.out.println("Input two strings");
+        String first = reader.nextLine();
+        String second = reader.nextLine();
         
-        if (number % 400 == 0) {
-            System.out.println("The number " + number + " is divisible by four hundred.");
+        if (first.equals(second)) {
+            System.out.println("The strings were the same!");
         } else {
-            System.out.println("The number " + number + " is not divisible by four hundred.");
+            System.out.println("The strings were different!");
         }
+        
+        if (first.equals("two strings")) {
+            System.out.println("Clever!");
+        } 
+        
+        if (second.equals("two strings")) {
+            System.out.println("Sneaky!");
+        }
+        
     }
 }
