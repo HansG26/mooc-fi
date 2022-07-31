4334a0674e1ee1d1a51c3e40eb373337f4da4866
Hans Goos <gooshans26@gmail.com>
2022-07-31 10:02:54 +0200

    complete exercise part02-Part02_14.Counting

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -6,33 +6,12 @@ public class Sandbox {
     public static void main(String[] args) {
         // Creating the scanner
         Scanner reader = new Scanner(System.in);
-
-        int numberOfPositives = 0;
-        int numberOfNegatives = 0;
-
-        while (true) {
-            System.out.println("Give a number (0 to stop): ");
-            int numberFromUser = Integer.valueOf(reader.nextLine());
-
-            if (numberFromUser == 0) {
-                break;
-            }
-
-            if (numberFromUser > 0) {
-                numberOfPositives = numberOfPositives + 1;
-            }
-
-            if (numberFromUser < 0) {
-                numberOfNegatives = numberOfNegatives + 1;
-            }
-        }
-
-        System.out.println("Positive numbers: " + numberOfPositives);
-        System.out.println("Negative numbers: " + numberOfNegatives);
-
-        if (numberOfPositives + numberOfNegatives > 0) {
-            double percentageOfPositives = 100.0 * numberOfPositives / (numberOfPositives + numberOfNegatives);
-            System.out.println("Percentage of positive numbers: " + percentageOfPositives + "%");
+        
+        int start = 3;
+        int end = 7;
+        
+        for (int i = start; i < end; i++) {
+            System.out.println(i);
         }
     }
 
