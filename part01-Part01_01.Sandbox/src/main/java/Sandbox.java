070a644799b5f197acb409389e686abd39059c20
Hans Goos <gooshans26@gmail.com>
2022-08-01 07:35:37 +0200

    complete exercise part02-Part02_25.Division

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -4,19 +4,16 @@ import java.util.Scanner;
 public class Sandbox {
 
     public static void main(String[] args) {
-        greet(1 + 2);
-        System.out.println("");
-        greet(3);
+        
+        int number1 = 2;
+        int number2 = 4;
+
+        sum(2, 4);    
         
     }
     
-    public static void greet(int numOfTimes) {
-        int i = 0;
-        
-        while(i < numOfTimes) {
-            System.out.println("Greetings!");
-            i++;
-        }
+    public static void sum(int first, int second) {
+        System.out.println("The sum of numbers " + first + " and " + second + " is " + (first + second));
     }
     
 }
