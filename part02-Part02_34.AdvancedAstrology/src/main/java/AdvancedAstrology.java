1e6d7ec0fc1f8928f8ab9a4a1b6c70faf8b15d20
Hans Goos <gooshans26@gmail.com>
2022-08-02 16:17:58 +0200

    complete exercise part 1 of part02-Part02_34.StarSign

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -3,10 +3,18 @@ public class AdvancedAstrology {
 
     public static void printStars(int number) {
         // part 1 of the exercise
+        for (int i = 1; i <= number; i++) {
+            System.out.print("*");
+        }
+
+        System.out.println("");
     }
 
     public static void printSpaces(int number) {
         // part 1 of the exercise
+        for (int i = 0; i < number; i++) {
+            System.out.print(" ");
+        }
     }
 
     public static void printTriangle(int size) {
