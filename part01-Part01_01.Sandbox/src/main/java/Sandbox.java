dbcfa4e559ea47feb18844a139575367a3ed8c04
Hans Goos <gooshans26@gmail.com>
2022-08-03 07:43:10 +0200

    complete exercise part03-Part03_04.ListSize

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -5,12 +5,9 @@ import java.util.ArrayList;
 public class Sandbox {
 
     public static void main(String[] args) {
-        ArrayList<String> wordList = new ArrayList<>();
+
         
-        wordList.add("first");
-        wordList.add("second");
         
-        System.out.println(wordList.get(1));
     }
 
 }
--- part03-Part03_02.SecondPlusThird/src/main/java/SecondPlusThird.java
+++ part03-Part03_02.SecondPlusThird/src/main/java/SecondPlusThird.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class SecondPlusThird {
 
     public static void main(String[] args) {
-        ArrayList<String> wordList = new ArrayList<>();
-        
-        wordList.add("First");
-        wordList.add("Second");
-        
-        System.out.println(wordList.get(2));
+        Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 0) {
+                break;
+            }
+            
+            numbers.add(number);
+        }
+
+        System.out.println(numbers.get(1) + numbers.get(2));
     }
 }
