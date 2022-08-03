312b7416df19eb9c6f01c8e4fbdf39f63830c6b7
Hans Goos <gooshans26@gmail.com>
2022-08-03 07:26:29 +0200

    complete exercise part03-Part03_03.IndexOutOfBoundsException

--- part03-Part03_02.SecondPlusThird/src/main/java/SecondPlusThird.java
+++ part03-Part03_02.SecondPlusThird/src/main/java/SecondPlusThird.java
@@ -5,18 +5,12 @@ import java.util.Scanner;
 public class SecondPlusThird {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> wordList = new ArrayList<>();
+        
+        wordList.add("First");
+        wordList.add("Second");
+        
+        System.out.println(wordList.get(2));
 
-        ArrayList<Integer> numbers = new ArrayList<>();
-        while (true) {
-            int number = Integer.valueOf(scanner.nextLine());
-            if (number == 0) {
-                break;
-            }
-            
-            numbers.add(number);
-        }
-
-        System.out.println(numbers.get(1) + numbers.get(2));
     }
 }
