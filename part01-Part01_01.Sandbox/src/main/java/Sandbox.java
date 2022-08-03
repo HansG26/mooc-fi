50370388f7f0b9b871aae328774e39dda967860b
Hans Goos <gooshans26@gmail.com>
2022-08-04 00:17:59 +0200

    complete exercise part03-Part03_05.LastInList

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -6,8 +6,17 @@ public class Sandbox {
 
     public static void main(String[] args) {
 
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
         
-        
+        for (int i = numbers.size() - 1; i >= 0; i--) {
+            System.out.println(numbers.get(i));
+        }
+
     }
 
 }
