87572f075f8a6ac9bc872048f272effe6d29281f
Hans Goos <gooshans26@gmail.com>
2022-08-05 16:34:32 +0200

    complete exercise part03-Part03_16.Sum

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -5,23 +5,20 @@ import java.util.ArrayList;
 public class Sandbox {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(2);
-        list.add(1);
-
-        printSmallerThan(list, 3);
+        
     }
 
-    public static void printSmallerThan(ArrayList<Integer> numbers, int threshold) {
-        for (int number : numbers) {
-            if (number < threshold) {
-                System.out.println(number);
-            }
+    public static double average(ArrayList<Integer> numbers) {
+        if(numbers.size() == 0) {
+            return -1.0;
         }
+        
+        int sum = 0;
+        for (int number: numbers) {
+            sum += number;
+        }
+        
+        return (double) sum / (double) numbers.size();
     }
 
 }
