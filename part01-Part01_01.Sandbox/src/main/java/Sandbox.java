96f47bc6d040bc7b893c4db2a405b98bea60bab0
Hans Goos <gooshans26@gmail.com>
2022-08-07 08:54:44 +0200

    complete exercise part03-Part03_20.SumOfArray

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -5,21 +5,24 @@ import java.util.ArrayList;
 public class Sandbox {
 
     public static void main(String[] args) {
-        int[] numbers = new int[4];
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
         
-        numbers[0] = 42;
-        numbers[1] = 13;
-        numbers[2] = 12;
-        numbers[3] = 7;
+        listElements(numbers);
+    }
         
-        System.out.println("The array has " + numbers.length + " elements.");
-        
-        int index =  0;
-        while(index < numbers.length) {
-            System.out.println(numbers[index]);
+    public static void listElements(int[] integerArray) {
+        System.out.println("The elements of the array are: ");
+        int index = 0;
+        while(index < integerArray.length) {
+            int number = integerArray[index];
+            System.out.print(number + " ");
             index++;
         }
         
-    }
+        System.out.println(" ");
+    }    
 
 }
