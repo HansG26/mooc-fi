a67b42a537f0189d64fde0411efd179e2cad81ac
Hans Goos <gooshans26@gmail.com>
2022-08-08 13:40:22 +0200

    complete exercise part03-Part03_28.FirstWords

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -5,7 +5,18 @@ import java.util.ArrayList;
 public class Sandbox {
 
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
         
+        while (true) {
+            String input = reader.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] pieces = input.split(",");
+            System.out.println("Name: " + pieces[0] + ", age " + pieces[1]);
+        }
     }
         
 
