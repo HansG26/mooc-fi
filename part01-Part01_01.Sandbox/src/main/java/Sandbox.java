e136342ffeed497b95da5e4a917a9fb0b059546d
Hans Goos <gooshans26@gmail.com>
2022-08-08 14:29:17 +0200

    complete exercise part03-Part03_32.PersonalDetails

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -5,24 +5,9 @@ import java.util.ArrayList;
 public class Sandbox {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        int sum = 0;
-        int numberOfInputs = 0;
-        
-        while (true) {
-            String input = reader.nextLine();
-            if (input.equals("")) {
-                break;
-            }
-            
-            String[] parts = input.split(",");
-            sum = sum + Integer.valueOf(parts[1]);
-            numberOfInputs++;
-            
-        }
-        
-        
-        System.out.println("Sum of the ages is " + sum);
+        String word = "equisterian";
+        int length = word.length();
+        System.out.println("The length of the word " + word + " is " + length);
     }
         
 }
--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,37 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String longestName = "";
+        int longestLength = 0;
+        int numberOfInputs = 0;
+        int sum = 0;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] pieces = input.split(",");
+            
+            String currentName = pieces[0];
+            int currentLength = currentName.length();
+            
+            if (currentLength > longestLength) {
+                longestLength = currentLength;
+                longestName = currentName;
+            }
+            
+            sum += Integer.valueOf(pieces[1]);
+            numberOfInputs++;
+            
+        }
+        
+        System.out.println("Longest name: " + longestName);
+        
+        double avg = (double) sum / (double) numberOfInputs;
+        System.out.println("Average of the birth years: " + avg);
 
     }
 }
