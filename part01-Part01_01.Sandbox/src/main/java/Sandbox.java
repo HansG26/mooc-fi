61e365b3f7e087212e43971f065fa0d02a283905
Hans Goos <gooshans26@gmail.com>
2022-08-08 11:19:17 +0200

    complete exercise part03-Part03_23.PrintThrice

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -5,24 +5,14 @@ import java.util.ArrayList;
 public class Sandbox {
 
     public static void main(String[] args) {
-        int[] numbers = new int[3];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        numbers[2] = 3;
+        String greeting = "Hi ";
+        String name = "Lily";
+        String goodbye = " and see you later!";
         
-        listElements(numbers);
+        String phrase = greeting + name + goodbye;
+        
+        System.out.println(phrase);
     }
         
-    public static void listElements(int[] integerArray) {
-        System.out.println("The elements of the array are: ");
-        int index = 0;
-        while(index < integerArray.length) {
-            int number = integerArray[index];
-            System.out.print(number + " ");
-            index++;
-        }
-        
-        System.out.println(" ");
-    }    
 
 }
