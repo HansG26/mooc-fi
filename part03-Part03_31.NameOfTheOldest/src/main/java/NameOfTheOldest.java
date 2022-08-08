63acd9eb507efb5db2ff710b57cc2608dedfce5b
Hans Goos <gooshans26@gmail.com>
2022-08-08 14:14:25 +0200

    complete exercise part03-Part03_31.NameOfTheOldest

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,6 +6,24 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        String name = "";
+        int age = 0;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] pieces = input.split(",");
+            
+            if(Integer.valueOf(pieces[1]) > age) {
+                name = pieces[0];
+                age = Integer.valueOf(pieces[1]);
+            }
+        } 
+        
+        System.out.println("Name of the oldest: " + name);
     }
 }
