b0cbc3faa401fbdd0d52620ec2ae105ff58b4e63
Hans Goos <gooshans26@gmail.com>
2022-08-08 22:11:41 +0200

    complete exercise part04-Part04_04.Room

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -5,14 +5,7 @@ import java.util.ArrayList;
 public class Sandbox {
 
     public static void main(String[] args) {
-        ArrayList<Integer> integers = new ArrayList<>();
-        
-        integers.add(15);
-        integers.add(34);
-        integers.add(65);
-        integers.add(111);
-        
-        System.out.println(integers.size());
+        Person ada = new Person("Ada");
     }
         
 }
