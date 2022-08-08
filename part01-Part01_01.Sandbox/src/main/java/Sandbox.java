4ef764ecea25d784b01b5ec0d4bdb09bba0b1bec
Hans Goos <gooshans26@gmail.com>
2022-08-08 13:27:30 +0200

    complete exercise part03-Part03_26.LineByLine

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -5,14 +5,7 @@ import java.util.ArrayList;
 public class Sandbox {
 
     public static void main(String[] args) {
-        System.out.println("Make sure the text is not 'cake'");
-        String text = "pie";
         
-        if(!text.equals("cake")) {
-            System.out.println("it wasn't");
-        } else {
-            System.out.println("it was!");
-        }
     }
         
 
