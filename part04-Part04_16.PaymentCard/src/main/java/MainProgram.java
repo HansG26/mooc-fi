600f66f598cc0b23e5d3591fe954e989d649f99b
Hans Goos <gooshans26@gmail.com>
2022-08-10 19:14:40 +0200

    complete exercise part 6 of part04-Part04_16.PaymentCard

--- part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
+++ part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
@@ -1,18 +1,32 @@
 
 import java.util.Scanner;
 
-
 public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        PaymentCard card = new PaymentCard(10);
-        System.out.println("Paul: " + card);
-        
-        card.addMoney(-15);
-        System.out.println("Paul: " + card);
 
+        PaymentCard cardPaul = new PaymentCard(20);
+        PaymentCard cardMatt = new PaymentCard(30);
 
+        cardPaul.eatHeartily();
+        cardMatt.eatAffordably();
+
+        System.out.println("Paul: " + cardPaul);
+        System.out.println("Matt: " + cardMatt);
+
+        cardPaul.addMoney(20);
+        cardMatt.eatHeartily();
+
+        System.out.println("Paul: " + cardPaul);
+        System.out.println("Matt: " + cardMatt);
+
+        cardPaul.eatAffordably();
+        cardPaul.eatAffordably();
+
+        cardMatt.addMoney(50);
+
+        System.out.println("Paul: " + cardPaul);
+        System.out.println("Matt: " + cardMatt);
     }
 }
