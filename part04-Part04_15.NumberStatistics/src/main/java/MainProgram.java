00ed08dbc63d01a5bb91ac16019e3d5c487969f3
Hans Goos <gooshans26@gmail.com>
2022-08-10 07:44:58 +0200

    complete exercise part 1 of part04-Part04_15.NumberStatistics

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -11,6 +11,6 @@ public class MainProgram {
         statistics.addNumber(5);
         statistics.addNumber(1);
         statistics.addNumber(2);
-        System.out.println("Count: " + statistics.getCount());
+        System.out.println("Count: " + statistics.getCount);
     }
 }
