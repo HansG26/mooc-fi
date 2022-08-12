0602d7e7f301d4c87b75fbf11c9fd10cf4ac667f
Hans Goos <gooshans26@gmail.com>
2022-08-12 19:45:13 +0200

    complete exercise part04-Part04_27.IsItInTheFile

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -12,6 +12,28 @@ public class IsItInTheFile {
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+        
+        boolean wasFound = false;
+        
+        try (Scanner reader = new Scanner(Paths.get(file))) {
+            while(reader.hasNextLine()) {
+                String line = reader.nextLine();
+                
+                if (line.equals(searchedFor)) {
+                    wasFound = true;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+            return;
+        }
+        
+        if (wasFound) {
+            System.out.println("Found!");
+        } else {
+            System.out.println("Not found.");
+        }
 
     }
 }
