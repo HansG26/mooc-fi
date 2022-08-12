64dfb571e3782b4beec92094fc8c0170f11b8c30
Hans Goos <gooshans26@gmail.com>
2022-08-12 06:24:33 +0200

    complete exercise part04-Part04_21.NumberOfStrings

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -7,35 +7,15 @@ public class Sandbox {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Person> persons = new ArrayList<>();
         
         while (true) {
-            System.out.println("Enter the details, empty will stop: ");
-            String details = scanner.nextLine();
-            if (details.isEmpty()) {
+            String line = scanner.nextLine();
+            
+            if (line.equals("end")) {
                 break;
             }
-            
-            String[] parts = details.split(",");
-            
-            String name = parts[0];
-            int age = Integer.valueOf(parts[1]);
-            
-            persons.add(new Person(name, age));
         }
-        
-        System.out.println("");
-        System.out.println("Persons in total: " + persons.size());
-        System.out.println("Persons: ");
-        
-        System.out.println("What is the age limit?");
-        int ageLimit = Integer.valueOf(scanner.nextLine());
-        
-        for(Person person : persons) {
-            if(person.getAge() >= ageLimit) {
-                System.out.println(person);
-            }
-        }
+
     }
         
 }
--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,19 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int numberOfInputs = 0;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if(input.equals("end")) {
+                break;
+            }
+            
+            numberOfInputs++;
+        }
+        
+        System.out.println(numberOfInputs);
     }
 }
