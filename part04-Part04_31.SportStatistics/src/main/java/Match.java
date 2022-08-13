487cbe207eab9fcac220dd2418c5aa016b0548f2
Hans Goos <gooshans26@gmail.com>
2022-08-13 08:41:25 +0200

    complete exercise part 1 of part04-Part04_31.SportStatistics

---
+++ part04-Part04_31.SportStatistics/src/main/java/Match.java
@@ -0,0 +1,28 @@
+
+public class Match {
+    
+    private String homeTeam;
+    private String visitingTeam;
+    private int homePoints;
+    private int awayPoints;
+    
+    public Match(String homeTeam, String visitingTeam, int homePoints, int awayPoints) {
+        this.homeTeam = homeTeam;
+        this.visitingTeam = visitingTeam;
+        this.homePoints = homePoints;
+        this.awayPoints = awayPoints;
+    }
+    
+    public String getHomeTeam() {
+        return this.homeTeam;
+    }
+    
+    public String getVisitingTeam() {
+        return this.visitingTeam;
+    }
+    
+    @Override
+    public String toString() {
+        return this.homeTeam + ", " + this.visitingTeam + ", " + this.homePoints + ", " + this.awayPoints;
+    }
+}
--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -7,7 +7,49 @@ public class SportStatistics {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        
+        System.out.println("File:");
+        String file = scan.nextLine();
+        
+        ArrayList<Match> matches = readMatchStatistics(file);
+        
+        System.out.println("Team: ");
+        String team = scan.nextLine();
+        
+        int games = 0;
+        
+        for (Match match: matches) {
+            if (match.getHomeTeam().equals(team) || match.getVisitingTeam().equals(team)) {
+                games++;
+            }
+        }
+        
+        System.out.println("Games: " + games);
+        
+    }
+    
+    public static ArrayList<Match> readMatchStatistics(String file) {
+        
+        ArrayList<Match> matches = new ArrayList<>();
+        
+        try (Scanner scanner = new Scanner(Paths.get(file))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                
+                String[] parts = line.split(",");
+                
+                String homeTeam = parts[0];
+                String awayTeam = parts[1];
+                int homePoints = Integer.valueOf(parts[2]);
+                int awayPoints = Integer.valueOf(parts[3]);
+                
+                matches.add(new Match(homeTeam, awayTeam, homePoints, awayPoints));
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+        return matches;
     }
 
 }
