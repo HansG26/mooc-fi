45c7ee0aaaa86fa404315b532475f3ec61e223c4
Hans Goos <gooshans26@gmail.com>
2022-08-13 08:00:23 +0200

    complete exercise part04-Part04_29.RecordsFromAFile

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -6,18 +6,37 @@ import java.util.ArrayList;
 public class Sandbox {
 
     public static void main(String[] args) {
-
-        ArrayList<String> lines = new ArrayList<>();
         
-        try (Scanner scanner = new Scanner(Paths.get("file.txt"))) {
-            while(scanner.hasNextLine()) {
-                lines.add(scanner.nextLine());
+        try (Scanner scanner = new Scanner(Paths.get("records.txt"))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.isEmpty()) {
+                    continue;
+                }
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
             }
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
         
-        System.out.println("Total lines: " + lines.size());
+        while (true) {
+            System.out.println("Enter name and age seperated by a comma: ");
+            String line = scanner.nextLine();
+            
+            if (line.equals("")) {
+                break;
+            }
+            
+            String[] parts = line.split(",");
+            String name = parts[0];
+            int age = Integer.valueOf(parts[1]);
+            
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+        }
+
     }
         
 }
--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,27 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        System.out.println("Name of the file:");
+        String input = scanner.nextLine();
+        
+        try (Scanner reader = new Scanner(Paths.get(input))) {
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+                
+                if (age == 1) {
+                    System.out.println(name + ", age: " + age + " year");
+                } else {
+                    System.out.println(name + ", age: " + age + " years");
+                }
+                
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
