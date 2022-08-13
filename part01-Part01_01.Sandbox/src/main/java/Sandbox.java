899ec910160ac0473717231159b3b112666d3160
Hans Goos <gooshans26@gmail.com>
2022-08-13 08:10:44 +0200

    complete exercise part04-Part04_30.StoringRecords

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -7,36 +7,21 @@ public class Sandbox {
 
     public static void main(String[] args) {
         
+        ArrayList<Person> people = new ArrayList<>();
+        
         try (Scanner scanner = new Scanner(Paths.get("records.txt"))) {
-            while (scanner.hasNextLine()) {
+            while(scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                if (line.isEmpty()) {
-                    continue;
-                }
+                
                 String[] parts = line.split(",");
                 String name = parts[0];
                 int age = Integer.valueOf(parts[1]);
+                
+                people.add(new Person(name, age));
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        
-        while (true) {
-            System.out.println("Enter name and age seperated by a comma: ");
-            String line = scanner.nextLine();
-            
-            if (line.equals("")) {
-                break;
-            }
-            
-            String[] parts = line.split(",");
-            String name = parts[0];
-            int age = Integer.valueOf(parts[1]);
-            
-            System.out.println("Name: " + name);
-            System.out.println("Age: " + age);
-        }
-
     }
         
 }
