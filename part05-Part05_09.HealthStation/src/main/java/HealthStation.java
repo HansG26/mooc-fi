895bd7f0848cac0895dfa440bc07359a8d1ecbf2
Hans Goos <gooshans26@gmail.com>
2022-08-15 11:44:52 +0200

    complete exercise part05-Part05_09.HealthStation

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,16 @@
 
 public class HealthStation {
-
+    
+    private int weighings;
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
+        this.weighings++;
         return person.getWeight();
+    }
+    
+    public int weighings() {
+        return this.weighings;
     }
     
     public void feed(Person person) {
