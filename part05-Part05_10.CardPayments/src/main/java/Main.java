f7405653d22004be9efc900188dbd3100fc62863
Hans Goos <gooshans26@gmail.com>
2022-08-16 07:28:23 +0200

    complete exercise part05-Part05_10.CardPayments

--- part05-Part05_10.CardPayments/src/main/java/Main.java
+++ part05-Part05_10.CardPayments/src/main/java/Main.java
@@ -3,6 +3,16 @@ public class Main {
 
     public static void main(String[] args) {
         // write experimental main programs here
+        PaymentCard petesCard = new PaymentCard(10);
+        
+        System.out.println("money " + petesCard.balance());
+        boolean wasSuccesful = petesCard.takeMoney(8);
+        System.out.println("successfully withdrew: " + wasSuccesful);
+        System.out.println("money " + petesCard.balance());
+        
+        wasSuccesful = petesCard.takeMoney(4);
+        System.out.println("succesfuly withdrew: " + wasSuccesful);
+        System.out.println("money " + petesCard.balance());
     }
 }
 
